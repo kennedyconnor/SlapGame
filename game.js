2c33b0eecf2567cf179c989f2efb2574fa57b9cf
Connor Kennedy <kennedyconnor02@gmail.com>
2019-05-10 17:19:34 -0600

    working copy

--- game.js
+++ game.js
@@ -1,22 +1,34 @@
 let health = 100;
+let pName = '';
+let hits = 0;
 
-drawHealth()
+drawUI()
 
 function punch() {
   health--;
-  drawHealth();
+  hits++;
+  drawUI();
 }
 
 function kick() {
   health = health - 5;
-  drawHealth();
+  hits++;
+  drawUI();
 }
 
 function burnKnuckle() {
   health = health - 10;
-  drawHealth();
+  hits++;
+  drawUI();
 }
 
-function drawHealth() {
-  document.getElementById('health').innerText = 'Rock Health: ' + health;
+function drawUI() {
+  if (health >= 0) {
+    document.getElementById('health').innerText = 'Rock Howard: ' + health;
+    document.getElementById('hits').innerText = 'Hits: ' + hits;
+  }
+  if (health == 0) {
+    document.getElementById('rock-sprite').setAttribute('src', 'assets/rock-fall.gif');
+  }
+
 }
