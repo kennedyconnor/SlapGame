b98cbf1591a0645def355b58b72ea1f30b8e9e40
Connor Kennedy <kennedyconnor02@gmail.com>
2019-05-13 02:41:45 -0600

    animaton final

--- game.js
+++ game.js
@@ -1,37 +1,70 @@
-let health = 100;
-let pName = '';
-let hits = 0;
+let dummy = {
+  health: 100,
+  name: 'Rock Howard',
+  hits: 0,
+  isAlive: true,
+  hitState: ['blocking', 'counterhit', 'guardbreak'],
+  animation: ['assets/rock-idle.gif', 'assets/rock-fall.gif'],
+}
+let player = {
+  health: 100,
+  name: 'Terry Bogard',
+  hits: 0,
+  isAlive: true,
+  hitState: ['blocking', 'counterhit', 'guardbreak'],
+  animation: ['assets/terry-idle.gif', 'assets/terry-punch.gif', 'assets/terry-kick.gif', 'assets/terry-burnKnuckle.gif', 'assets/terry-win3.gif'],
+  animationHeight: ['140px', , , , '280px'],
+  animationLeft: ['150px', , , , '120px'],
+}
 
-console.log("hello dumbass")
 drawUI()
 
 function punch() {
-  health--;
-  hits++;
+  //document.getElementById('terry-sprite').setAttribute('src', player.animation[1]); 
+  dummy.health--;
+  dummy.hits++;
   drawUI();
 }
 
 function kick() {
-  health = health - 5;
-  hits++;
+  //document.getElementById('terry-sprite').setAttribute('src', player.animation[2]);
+  dummy.health = dummy.health - 5;
+  dummy.hits++;
   drawUI();
 }
 
 function burnKnuckle() {
-  health = health - 10;
-  hits++;
+  //document.getElementById('terry-sprite').setAttribute('src', player.animation[3]);
+  dummy.health = dummy.health - 10;
+  dummy.hits++;
   drawUI();
 }
 
 function drawUI() {
-  if (health > 0) {
-    document.getElementById('health').innerText = 'Rock Howard: ' + health;
-    document.getElementById('hits').innerText = 'Hits: ' + hits;
+  if (dummy.health > 0) {
+    document.getElementById('health').innerText = 'Health: ' + dummy.health;
+    document.getElementById('hits').innerText = 'Hits: ' + dummy.hits;
+    document.getElementById('name').innerText = 'Dummy: ' + dummy.name;
   }
-  else if (health <= 0) {
-    document.getElementById('health').innerText = 'Rock Howard:' + health;
-    document.getElementById('rock-sprite').setAttribute('src', 'assets/rock-fall.gif');
-    document.getElementById('hits').innerText = 'Hits: ' + hits;
+  if (dummy.health <= 0 && dummy.isAlive) {
+    document.getElementById('health').innerText = 'Health: ' + 0;
+    document.getElementById('rock-sprite').setAttribute('src', dummy.animation[1]);
+    document.getElementById('terry-sprite').setAttribute('src', player.animation[4]);
+    document.getElementById('terry-sprite').style.height = player.animationHeight[4];
+    document.getElementById('terry-sprite').style.left = player.animationLeft[4];
+    document.getElementById('hits').innerText = 'Hits: ' + dummy.hits;
+    dummy.isAlive = false;
   }
 
+}
+
+function reset() {
+  dummy.health = 100;
+  dummy.isAlive = true;
+  dummy.hits = 0;
+  document.getElementById('rock-sprite').setAttribute('src', dummy.animation[0]);
+  document.getElementById('terry-sprite').setAttribute('src', player.animation[0]);
+  document.getElementById('terry-sprite').style.height = player.animationHeight[0];
+  document.getElementById('terry-sprite').style.left = player.animationLeft[0];
+  drawUI();
 }
